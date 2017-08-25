dbf20ccd3e350aac6ca014f00d076492b8f9bf70
Samuel <slauric1@gmail.com>
2017-08-24 21:02:10 -0400

    Flash error with basic routing

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
+const blogsController = require('../controllers/blogsController')
+const {catchErrors} = require('../handlers/errorHandlers')
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'The Political Zone' });
-});
-
+router.get('/', blogsController.getArticles)
+router.get('/add',blogsController.addArticle)
+router.post('/add', blogsController.createArticle)
 module.exports = router;
